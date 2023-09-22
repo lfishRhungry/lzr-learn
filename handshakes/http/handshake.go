ffee2a14465bbe04eb647c6316d6ad108a49009b
lfishRhungry <shineccy@aliyun.com>
2023-09-22 09:00:48 +0800

    update user-agent in http handshake

--- handshakes/http/handshake.go
+++ handshakes/http/handshake.go
@@ -1,9 +1,10 @@
 package http
 
 import (
-    "net/http"
-    "net/http/httputil"
+	"net/http"
+	"net/http/httputil"
 	"strings"
+
 	"github.com/stanford-esrg/lzr"
 )
 
@@ -11,24 +12,24 @@ import (
 type HandshakeMod struct {
 }
 
-func (h *HandshakeMod) GetData( dst string ) []byte {
+func (h *HandshakeMod) GetData(dst string) []byte {
 
-        req, _ := http.NewRequest("GET","/",nil)
-        req.Host =  dst
-        req.Header.Add("Host",dst)
-        req.Header.Set("User-Agent","Mozilla/5.0 zgrab/0.x")
-        req.Header.Set("Accept","*/*")
-        req.Header.Set("Accept-Encoding","gzip")
-        data, _ := httputil.DumpRequest(req, false)
-    return data
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Host = dst
+	req.Header.Add("Host", dst)
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36 Edg/114.0.1823.43")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Accept-Encoding", "gzip")
+	data, _ := httputil.DumpRequest(req, false)
+	return data
 }
 
-func (h *HandshakeMod) Verify( data string ) string {
+func (h *HandshakeMod) Verify(data string) string {
 
-	if !strings.Contains( data, "HTTPS" ) &&
-		(strings.Contains( data, "HTTP" ) || strings.Contains( data, "html" ) ||
-		strings.Contains( data, "HTML") || strings.Contains( data, "<h1>" )) {
-         return "http"
+	if !strings.Contains(data, "HTTPS") &&
+		(strings.Contains(data, "HTTP") || strings.Contains(data, "html") ||
+			strings.Contains(data, "HTML") || strings.Contains(data, "<h1>")) {
+		return "http"
 	}
 	return ""
 
@@ -36,6 +37,5 @@ func (h *HandshakeMod) Verify( data string ) string {
 
 func RegisterHandshake() {
 	var h HandshakeMod
-	lzr.AddHandshake( "http", &h )
+	lzr.AddHandshake("http", &h)
 }
-
