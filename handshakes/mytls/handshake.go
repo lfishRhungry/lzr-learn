a15d1b864f996772d38bf026e2c2736eb7e0ce56
lfishRhungry <shineccy@aliyun.com>
2023-10-14 18:09:52 +0800

    use raw way to construct ClientHello for more flexible using

--- handshakes/mytls/handshake.go
+++ handshakes/mytls/handshake.go
@@ -14,21 +14,98 @@ var clienHelloTemplate []byte
 
 func init() {
 
+	//modifying based on ClientHello of Chrome102 a
+	spec := &tls.ClientHelloSpec{
+		CipherSuites: []uint16{
+			tls.GREASE_PLACEHOLDER,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+		CompressionMethods: []byte{
+			0x00, // compressionNone
+		},
+		Extensions: []tls.TLSExtension{
+			&tls.UtlsGREASEExtension{},
+			&tls.SNIExtension{},
+			&tls.ExtendedMasterSecretExtension{},
+			&tls.RenegotiationInfoExtension{Renegotiation: tls.RenegotiateOnceAsClient},
+			&tls.SupportedCurvesExtension{Curves: []tls.CurveID{
+				tls.GREASE_PLACEHOLDER,
+				tls.X25519,
+				tls.CurveP256,
+				tls.CurveP384,
+			}},
+			&tls.SupportedPointsExtension{SupportedPoints: []byte{
+				0x00, // pointFormatUncompressed
+			}},
+			&tls.SessionTicketExtension{},
+			//!NOTE
+			&tls.ALPNExtension{AlpnProtocols: []string{"h2", "http/1.1", "pop3", "imap", "ftp"}},
+			&tls.StatusRequestExtension{},
+			&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
+				tls.ECDSAWithP256AndSHA256,
+				tls.PSSWithSHA256,
+				tls.PKCS1WithSHA256,
+				tls.ECDSAWithP384AndSHA384,
+				tls.PSSWithSHA384,
+				tls.PKCS1WithSHA384,
+				tls.PSSWithSHA512,
+				tls.PKCS1WithSHA512,
+			}},
+			&tls.SCTExtension{},
+			&tls.KeyShareExtension{KeyShares: []tls.KeyShare{
+				{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
+				{Group: tls.X25519},
+			}},
+			&tls.PSKKeyExchangeModesExtension{Modes: []uint8{
+				tls.PskModeDHE,
+			}},
+			&tls.SupportedVersionsExtension{Versions: []uint16{
+				tls.GREASE_PLACEHOLDER,
+				tls.VersionTLS13,
+				tls.VersionTLS12,
+			}},
+			&tls.UtlsCompressCertExtension{Algorithms: []tls.CertCompressionAlgo{
+				tls.CertCompressionBrotli,
+			}},
+			&tls.ApplicationSettingsExtension{SupportedProtocols: []string{"h2"}},
+			&tls.UtlsGREASEExtension{},
+			&tls.UtlsPaddingExtension{GetPaddingLen: tls.BoringPaddingStyle},
+		},
+	}
 	// ServerName is used to verify the hostname on the returned
 	// certificates unless InsecureSkipVerify is given. It is also included
 	// in the client's handshake to support virtual hosting unless it is
 	// an IP address.
 	// But we do not know hostname from an IP address, so...
-	tc := tls.UClient(nil, &tls.Config{InsecureSkipVerify: true}, tls.HelloChrome_Auto)
-
-	err := tc.BuildHandshakeState()
+	tClient := tls.UClient(nil, &tls.Config{InsecureSkipVerify: true}, tls.HelloCustom)
+	err := tClient.ApplyPreset(spec)
 	if err != nil {
 		fmt.Println(err)
+		return
+	}
+	err = tClient.BuildHandshakeState()
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
 	//Construct full TLS ClientHello data for tcp payload
 	var b [2]byte
-	l := uint16(len(tc.HandshakeState.Hello.Raw))
+	l := uint16(len(tClient.HandshakeState.Hello.Raw))
 	binary.BigEndian.PutUint16(b[0:2], l)
 	clienHelloTemplate = append( //Need to wrap Handshake payload with TLS Record Layer
 		[]byte{
@@ -36,7 +113,7 @@ func init() {
 			0x03, 0x01, //Version: TLS 1.0 (0x0301)
 			b[0], b[1], //Length
 		},
-		tc.HandshakeState.Hello.Raw...)
+		tClient.HandshakeState.Hello.Raw...)
 
 	//Inject '\r\n' to random of ClientHello manually.
 	//this make http1.1 verified by http response for bad request
@@ -155,6 +232,33 @@ func (h *HandshakeMod) Verify(data string) string {
 			0x68, 0x32, 0x63, //ALPN Next Protocol: h2c
 		}) {
 		ret = "http2tcp"
+	} else if bytes.Contains(datab,
+		[]byte{
+			0x00, 0x10, //Extension Type: ALPN
+			0x00, 0x07, //Length: 7
+			0x00, 0x05, //ALPN Extension Length: 5
+			0x04,                   //ALPN string length: 4
+			0x70, 0x6f, 0x70, 0x33, //ALPN Next Protocol: pop3
+		}) {
+		ret = "pop3s"
+	} else if bytes.Contains(datab,
+		[]byte{
+			0x00, 0x10, //Extension Type: ALPN
+			0x00, 0x07, //Length: 7
+			0x00, 0x05, //ALPN Extension Length: 5
+			0x04,                   //ALPN string length: 4
+			0x69, 0x6d, 0x6d, 0x70, //ALPN Next Protocol: imap
+		}) {
+		ret = "imaps"
+	} else if bytes.Contains(datab,
+		[]byte{
+			0x00, 0x10, //Extension Type: ALPN
+			0x00, 0x06, //Length: 6
+			0x00, 0x04, //ALPN Extension Length: 4
+			0x03,             //ALPN string length: 3
+			0x66, 0x74, 0x70, //ALPN Next Protocol: ftp
+		}) {
+		ret = "ftps"
 	}
 
 	// return ""
